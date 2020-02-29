4d50289116e05a687ca8c87fff7ec2c9036a26bf
m00ns7ruck <m00ns7ruck@abv.bg>
2020-02-29 21:38:11 +0200

    Script to lock screen now suspends too

--- i3_lock_tty.sh
+++ i3_lock_tty.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-/usr/bin/physlock -l              \
-    & /usr/bin/i3lock             \
-	  --ignore-empty-password \
-	  --color='#1c1721'       \
-	  -n;
-/usr/bin/physlock -L
+/usr/bin/physlock -l &
+/usr/bin/i3lock             \
+    --ignore-empty-password \
+    --color='#1c1721'       \
+    -n &
+/bin/systemctl suspend;
+/usr/bin/physlock -L;
